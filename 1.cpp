bf295a90803bf85b53018e5be08d6a0010b1a93f
ssrishti582 <ssrishti582@gmail.com>
2023-11-06 11:54:01 +0530

    new change added

--- 1.cpp
+++ 1.cpp
@@ -3,12 +3,9 @@ using namespace std;
 void quicksort(){
     
 }
-<<<<<<< HEAD
 void bubblesort(){
     
 }
-=======
->>>>>>> quicksort
 int main(){
     cout<<"hello "<<endl;
     cout<<"hello dot batch"<<endl;
